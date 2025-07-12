53890aeecda5f545390f12a3e39f91b22d910d06
Lovnish Verma <princelv84@gmail.com>
2025-07-13 01:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,42 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send
 from pymongo import MongoClient
 from datetime import datetime
 import pytz
 import os
 
-# Setup Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-
-# Setup SocketIO
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# Setup MongoDB
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/chatdb?retryWrites=true&w=majority')
+# MongoDB Atlas URI
+MONGO_URI = os.environ.get('MONGO_URI')  # will set from Render Dashboard
 client = MongoClient(MONGO_URI)
-db = client.get_database("chatdb")
-messages_collection = db.get_collection("messages")
+db = client['chat_db']
+messages = db['messages']
 
-# Timezone for IST
-IST = pytz.timezone('Asia/Kolkata')
+# IST Timezone
+tz = pytz.timezone('Asia/Kolkata')
 
 @app.route('/')
 def index():
-    # Fetch messages from MongoDB
-    messages = list(messages_collection.find({}, {"_id": 0}))
-    return render_template('index.html', messages=messages)
+    all_messages = list(messages.find({}, {'_id': 0}))
+    return render_template('index.html', messages=all_messages)
 
-@socketio.on('send_message')
-def handle_send_message(data):
-    nickname = data.get('nickname')
-    message = data.get('message')
-    timestamp = datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')
+@socketio.on('message')
+def handle_message(msg):
+    nickname = msg['nickname']
+    message_text = msg['message']
+    timestamp = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
     
-    full_message = {
-        "nickname": nickname,
-        "message": message,
-        "timestamp": timestamp
+    chat_message = {
+        'nickname': nickname,
+        'message': message_text,
+        'timestamp': timestamp
     }
-
-    # Save to MongoDB
-    messages_collection.insert_one(full_message)
     
-    # Broadcast to all clients
-    emit('receive_message', full_message, broadcast=True)
+    messages.insert_one(chat_message)
+    send(chat_message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host='0.0.0.0', port=5000)
