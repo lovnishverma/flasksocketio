f8f33aa6504ace98f20b1ab2431c2c609f1cd077
Lovnish Verma <princelv84@gmail.com>
2025-07-13 17:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,6 @@ def handle_message(data):
     emit('message', message_doc, broadcast=True)
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port)
